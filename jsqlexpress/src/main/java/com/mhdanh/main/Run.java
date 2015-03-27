56db01a7ec6c70705b647c0ffb033d0b26522734
maidanhcongtu@gmail.com <maidanhcongtu@gmail.com>
2015-03-27 15:29:14 +0700

    change database to jsqlexpress

--- jsqlexpress/src/main/java/com/mhdanh/main/Run.java
+++ jsqlexpress/src/main/java/com/mhdanh/main/Run.java
@@ -7,20 +7,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.mhdanh.interfaces.Crud;
 
 public class Run {
+	@SuppressWarnings("resource")
+	
 	public static void main(String[] args) {
-try {
-			
-//			String path = RunMain.class.getClassLoader().getResource("application-context.xml").toExternalForm();
-//	        JOptionPane.showMessageDialog(null, path);
-			
-			//URL location = Main.class.getProtectionDomain().getCodeSource().getLocation();
-			//JOptionPane.showMessageDialog(null,"Path: " + location.getFile());
-			ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("application-context.xml");
+		try {
+			ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
+					"application-context.xml");
 			Crud crud = ctx.getBean(Crud.class);
 			crud.setVisible(true);
 			crud.setSize(500, 400);
 		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null,"error:" + e);
+			JOptionPane.showMessageDialog(null, "error:" + e);
 		}
 	}
 }
